b7cd4134ccb4501c84688437e7e554fb9ce8d1e9
Michael <mikenegh@gmail.com>
2025-01-06 20:25:26 +0300

    Params added

--- location.py
+++
@@ -1,3 +0,0 @@
-latitude = 9.03
-longitude = 38.74
-maxradiuskm = 2000
---
+++ params.py
@@ -0,0 +1,13 @@
+PARAMS = {
+    "format": "geojson",  # Output format (options: geojson, csv, kml, etc.)
+    "starttime": "2025-01-01",  # Start date in ISO8601 format
+    "endtime": "2025-01-06",  # End date in ISO8601 format
+    "minmagnitude": 5,  # Minimum magnitude
+    "maxmagnitude": 7,  # Maximum magnitude
+    "minlatitude": -90,  # Minimum latitude
+    "maxlatitude": 90,  # Maximum latitude
+    "minlongitude": -180,  # Minimum longitude
+    "maxlongitude": 180,  # Maximum longitude
+    "orderby": "time",  # Order results by time (descending)
+    "limit": 100,  # Limit results to 100 events
+}
